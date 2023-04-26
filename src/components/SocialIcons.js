f913bb7947871bcfb1c6603fcda1ae2addb20e82
Adeeb Abdulsamad <102668476+adeeb-samad@users.noreply.github.com>
2023-04-26 19:16:43 +0300

    Update SocialIcons.js

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,14 +1,14 @@
 const SocialIcons = () => {
   return (
     <div className="socialIcons">
-      <a className="icon" href="https://github.com/Adeeb2022">
-        <i className="fa-brands fa-github" aria-hidden="true" title="Adeeb'a GitHub Profile"></i>
+      <a className="icon" href="https://github.com/adeeb-samad">
+        <i className="fa-brands fa-github" aria-hidden="true" title="Adeeb's GitHub Profile"></i>
       </a>
       <a className="icon" href="https://www.linkedin.com/in/adeeb-samad/">
-        <i className="fa-brands fa-linkedin" aria-hidden="true" title="Adeeb'a LinkedIn Profile"></i>
+        <i className="fa-brands fa-linkedin" aria-hidden="true" title="Adeeb's LinkedIn Profile"></i>
       </a>
-      <a className="icon" href="https://www.instagram.com/michael.yeates/">
-        <i className="fa-brands fa-facebook" aria-hidden="true" title="Adeeb'a F Profile"></i>
+      <a className="icon" href="https://www.facebook.com/adeebsamed">
+        <i className="fa-brands fa-facebook" aria-hidden="true" title="Adeeb's Facebook Profile"></i>
       </a>
     </div>
   );
