9cb30fbd6f47e8626b7c274e2b2d170e9640c4cb
Adeeb Abdulwasea <102668476+adeeb-samad@users.noreply.github.com>
2025-04-14 20:16:03 +0300

    Update SocialIcons.js

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,14 +1,14 @@
 const SocialIcons = () => {
   return (
     <div className="socialIcons">
-      <a className="icon" href="https://github.com/adeeb-samad">
+      <a className="icon" aria-label="Visit my Github account" href="https://github.com/adeeb-samad">
         <i className="fa-brands fa-github" aria-label="Visit my Github account" title="Adeeb's GitHub Profile"></i>
       </a>
-      <a className="icon" href="https://www.linkedin.com/in/adeeb-samad/">
-        <i className="fa-brands fa-linkedin" aria-label="See my LinkedIn profile" title="Adeeb's LinkedIn Profile"></i>
+      <a className="icon" aria-label="See my LinkedIn profile" href="https://www.linkedin.com/in/adeeb-samad/">
+        <i className="fa-brands fa-linkedin" title="Adeeb's LinkedIn Profile"></i>
       </a>
-      <a className="icon" href="https://www.facebook.com/adeebsamed">
-        <i className="fa-brands fa-facebook" aria-label="Follow me on Facebook" title="Adeeb's Facebook Profile"></i>
+      <a className="icon" aria-label="Follow me on Facebook" href="https://www.facebook.com/adeebsamed">
+        <i className="fa-brands fa-facebook" title="Adeeb's Facebook Profile"></i>
       </a>
     </div>
   );
